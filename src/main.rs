8390cbc2f4e2bae3bbcd4680c8dad56a472a3f3a
Mustafa-Eldeeb <darshm466@gmail.com>
2022-01-01 00:40:16 +0200

    add attempt function(write the main algorithm)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,50 @@
 /* func to get common values in available and prefered lists */
-fn available_prefered(available:Vec<usize>,prefered:Vec<usize>)->Vec<usize>{
+fn available_prefered(available:Vec<usize>,preferred:Vec<usize>)->Vec<usize>{
 let mut res :Vec<usize>=vec![];
-for p in prefered{
+for p in preferred{
     if available.contains(&p){
         res.push(p)
     }
 }
 res
 }
+pub fn attempt(available:Vec<usize>,preferred:Vec<usize>,allowed:Vec<usize>)->Vec<usize>{
+    let mut res :Vec<usize>=vec![];
+    let mut available=available;
+    available.sort();
+    for p in preferred.iter(){
+        if available.contains(p){
+            res.push(*p)
+        }
+        else{
+            match closest_not_smaller(*p, &available) {
+                Some(val)=>res.push(val),
+                None=>{
+                    match closest_smaller(*p, &available) {
+                        Some(val)=>res.push(val),
+                        None=>()
+                    }
+                }
+            }
+            
+        }
+    }
+
+    allowed_result_list(res, allowed)   
+}
+
 
 //func to get the closest not smaller for prefered value
 
 fn main() {
-    println!("Hello, world!");
+
+ 
+    
+    let available=vec![720];
+    let allowed=vec![240,360,1080];
+    let preferred = vec![240,360];
+  let result=attempt(available, preferred, allowed);
+  println!("result {:#?}",result);
 }
 #[test]
 fn test_available_prefered(){
@@ -29,7 +61,7 @@ fn test_closest_not_smaller() {
     let available=vec![3,4,5,12,45,60];
     let num=4;
     let expected=5;
-    let res= closest_not_smaller(num, available);
+    let res= closest_not_smaller(num, &available).unwrap();
     println!("{:#?}",res);
     assert_eq!(expected,res);
 }
@@ -38,7 +70,7 @@ fn test_closest_smaller() {
     let available=vec![3,4,5,12,45,60];
     let num=12;
     let expected=5;
-    let res= closest_smaller(num, available);
+    let res= closest_smaller(num, &available).unwrap();
     println!("{:#?}",res);
     assert_eq!(expected,res);
 }
@@ -54,26 +86,26 @@ fn test_allowed_result_list() {
 }
 
 //func to get the available closest not smaller value to each value in prefered list
-fn closest_not_smaller(p_num:usize,available:Vec<usize>)->usize{
+fn closest_not_smaller(p_num:usize,available:&Vec<usize>)->Option<usize>{
     for e in available.iter() { 
         if e>&p_num
         {
-            return *e;
+            return Some(*e);
         }
     }
-0
+None
 
 }
 //func to get the available closest smaller value to each value in prefered list
-fn closest_smaller(p_num:usize,available:Vec<usize>)->usize{
+fn closest_smaller(p_num:usize,available:&Vec<usize>)->Option<usize>{
 
     for e in available.iter().rev() { 
         if e <&p_num 
         {
-            return *e;
+            return Some(*e);
         }
     }
-0
+None
 
 }
 
@@ -85,5 +117,82 @@ fn allowed_result_list(result:Vec<usize>,allowed:Vec<usize>)->Vec<usize>{
             confirmed_res.push(*res)
         }
     }
+    confirmed_res.dedup();
     confirmed_res
+
+}
+#[cfg(test)]
+mod tests{
+    use super::*;
+    
+    #[test]
+    fn test_attempt_1(){
+        let available=vec![240,360,720];
+        let allowed=vec![360,720];
+        let preferred = vec![1080];
+      let result = attempt(available, preferred, allowed);
+      println!("result {:#?}",result);
+    } 
+    #[test]
+    fn test_attempt_2(){
+        let available=vec![240,720];
+        let allowed=vec![360,720];
+        let preferred = vec![1080];
+      let result=attempt(available, preferred, allowed);
+      println!("result {:#?}",result);
+    }
+    #[test]
+    fn test_attempt_3(){
+        let available=vec![240];
+        let allowed=vec![360,720];
+        let preferred = vec![1080];
+      let result=attempt(available, preferred, allowed);
+      println!("result {:#?}",result);
+    }
+    #[test]
+    fn test_attempt_4(){
+        let available=vec![240,360,720];
+        let allowed=vec![240,360,720,1080];
+        let preferred = vec![240,360];
+      let result=attempt(available, preferred, allowed);
+      println!("result {:#?}",result);
+    }
+    
+    
+    #[test]
+    fn test_attempt_5(){
+        let available=vec![240,720];
+        let allowed=vec![240,360,720,1080];
+        let preferred = vec![240,360];
+      let result=attempt(available, preferred, allowed);
+      println!("result {:#?}",result);
+    }
+    #[test]
+    fn test_attempt_6(){
+        let available=vec![240,720];
+        let allowed=vec![240,360,1080];
+        let preferred = vec![240,360];
+      let result=attempt(available, preferred, allowed);
+      println!("result {:#?}",result);
+    }
+
+    #[test]
+    fn test_attempt_7(){
+        let available=vec![720];
+        let allowed=vec![240,360,1080];
+        let preferred = vec![240,360];
+      let result=attempt(available, preferred, allowed);
+      println!("result {:#?}",result);
+    }    
+    
+    #[test]
+    fn test_attempt_8(){
+        let available=vec![240,360];
+        let allowed=vec![240,360];
+        let preferred = vec![720,1080];
+      let result=attempt(available, preferred, allowed);
+      println!("result {:#?}",result);
+    }
+    
+   
 }
