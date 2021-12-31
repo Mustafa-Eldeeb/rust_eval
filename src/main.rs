78489f347ac3c9fa5ac28826e62e9f7a9984175c
Mustafa-Eldeeb <darshm466@gmail.com>
2021-12-31 20:39:13 +0200

    implement func that get prefered and available values

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
 
+
+/* func to get common values in available and prefered lists */
+fn available_prefered(available:Vec<usize>,prefered:Vec<usize>)->Vec<usize>{
+let mut res :Vec<usize>=vec![];
+for p in prefered{
+    if available.contains(&p){
+        res.push(p)
+    }
+}
+res
+}
+
+
+
 fn main() {
     println!("Hello, world!");
 }
+#[test]
+fn test_available_prefered(){
+   let prefered = vec![1,45,1240];
+    let available=vec![3,54,45,12,1];
+    let expected=vec![1,45];
+    let res=available_prefered(available, prefered);
+    println!("{:#?}",res);
+    assert_eq!(expected,res);
+}
+
+
